b75df66502af7b0e5943cae35b43315a835d9196
Bavly_Peter <bavlypeterb@gmail.com>
2025-12-13 19:28:44 +0200

    feat: Implement transportation management routes and templates; add transportation listing functionality and update Home Page navigation

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from routes.customer_routes import customer_bp
 from routes.product_routes import product_bp
 from routes.Suppliers_routes import suppliers_bp
 from routes.employee_routes import employee_bp
+from routes.transportation_routes import transportation_bp
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +19,7 @@ def create_app():
     app.register_blueprint(product_bp)
     app.register_blueprint(suppliers_bp)
     app.register_blueprint(employee_bp)
+    app.register_blueprint(transportation_bp)
 
     #Home Page
     @app.route('/')
